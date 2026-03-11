5550ac561cc02a24dc4dab73bf45f7bd4be738b8
TaoJiang-code <taojiang202511@163.com>
2026-03-11 21:06:19 +0800

    11

--- source/legged_lab/legged_lab/assets/qingyun_z1_A.py
+++ source/legged_lab/legged_lab/assets/qingyun_z1_A.py
@@ -33,7 +33,7 @@ qingyun_z1_A_CFG = qingyun_z1_A_ArticulationCfg(
         ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.8),
+        pos=(0.0, 0.0, 0.6),
         joint_pos={
             "w_waist_yaw": 0.0,
 
